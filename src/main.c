fa485de0fe7e03c2e2a07c62e76b1fe08709befa
Jack Bradach <jack@bradach.net>
2022-05-21 13:58:43 -0700

    Cleaned up a bunch of main.

--- src/main.c
+++ src/main.c
@@ -1,37 +1,23 @@
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+
 #include "config.h"
-#include "dbg.h"
-#include "uart.h"
-#include "spi.h"
-#include "neopixels.h"
 #include "dac.h"
+#include "dbg.h"
 #include "dht22.h"
-#include "twi_master.h"
 #include "ftseg.h"
-// #include "timer.h"
-
-#include "pixels.h"
-#if 0
+#include "neopixels.h"
 #include "spi.h"
-#include "pixels_anims.h"
-#include "pixels_mods.h"
-#include "neopixel_stick.h"
-#include "neopixel_avr.h"
-#include "led_gamma.h"
-
-neopixel_stick np_stick;
-
-pixels_anim anim_sine;
-
-
-pixels_anim anim_startup;
-#endif
+#include "uart.h"
+#include "twi_master.h"
+// #include "timer.h"
+#include <avr/wdt.h> 
 
 /* Application state; extend as needed! */
 // typedef enum {
@@ -49,7 +35,6 @@ typedef enum {
 } buttons_t;
 buttons_t get_buttons();
 
-neopixels_t np_chain;
 ftseg_device_handle_t *ftseg;
 
 // struct app_state {
@@ -59,26 +44,6 @@ ftseg_device_handle_t *ftseg;
 
 #define DELAY_LOOP 1000
 
-// ** Timers section **
-// TODO - move this section to timers.c!
-
-void timers_init() {
-    // Set up free-running timer for measuring microseconds.
-    // 16mHz / prescaler = 
-
-    // 16-bit running @ 20mHz, overflow at 20k -> 1khz.
-    // timer_ms_tick = 0;
-    // TCCR1A = 0;
-    // TCCR1B = _BV(WGM12) | _BV(CS10);
-    // OCR1A = 20000;
-    // TIMSK1 |= _BV(OCIE1A);
-
-    // TCCR0A = _BV(WGM01);
-    // TCCR0B = _BV(CS01);
-    // OCR0A = 250;
-
-}
-
 
 buttons_t get_buttons() {
     // TODO - Figure out which pins on the test board can have buttons.
@@ -87,67 +52,6 @@ buttons_t get_buttons() {
     // uint8_t buttons = BUTTON_PORT & BUTTON_MASK >> BUTTON_SHIFT;
 
     return BUTTONS_NONE;
-}
-
-
-# if 1
-int move_color(uint8_t *color, uint8_t target, int rate)
-{
-    if (target > *color) {
-        *color += rate;
-    } else if (target < *color) {
-        *color -= rate;
-    } else {
-        return 0;
-    }
-    return 1;
-}
-#else
-int move_color(uint8_t *color, uint8_t target, int rate)
-{
-    int c;
-    c = *color;
-
-    if (target > (c - rate)) {
-        c += rate;
-    } else if (target < c) {
-        c -= rate;
-    } - USB 
-    }
-    return 1;
-}
-#endif
-
-void anim_random_color_fader(neopixels_t *chain)
-{
-    uint8_t tgt_color[NEOPIXEL_CHAIN_LENGTH][3];
-    int rate = 1;
-
-    // 1. Random targets (per led)
-    // 2. Move color towards target.
-    // 3. If any colors are still moving, repeat.
-    // 4. loop
-    while (1) {
-        uint8_t done;
-        for (uint8_t i = 0; i < NEOPIXEL_CHAIN_LENGTH; i++) {
-            for (uint8_t c = 0; c < PIXELS_CH_MAX; c++) {
-                tgt_color[i][c] = rand();
-            }
-        }
-        do {
-            uint8_t i;
-            done = 1;
-            for (i = 0; i < NEOPIXEL_CHAIN_LENGTH; i++) {
-                for (uint8_t c = 0; c < 3; c++) {
-                    if (move_color((&(chain->color[i])[c]), tgt_color[i][c], rate)) {
-                        done = 0;
-                    }
-                }
-            }
-            neopixels_refresh(&np_chain);
-            _delay_ms(10.0);
-        } while (!done);
-    }
 }
 
 
@@ -161,11 +65,9 @@ static void init(void)
     // sdcard_init();
     twi_master_init();
 
-    // neopixels_init_chain(&np_chain, &NEOPIXEL_CHAIN_PORT,
-    //                        NEOPIXEL_CHAIN_PIN, NEOPIXEL_CHAIN_LENGTH);
+   
+    // wdt_enable(WDTO_2S);
     sei();
-    // printf("** Alive!! **\n");
-    
 }
 
 /* Micocontroller firmware entry point */
@@ -177,7 +79,6 @@ int main(void)
 
     printf("\n** Alive!! **\n");
     ftseg_init(&ftseg);
-    // anim_random_color_fader(&np_chain);
 
     /* Main application loop! */
     while (1) {
@@ -230,13 +131,6 @@ int main(void)
         ftseg_write_text(ftseg, 0, text);
         _delay_ms(DELAY_LOOP);
 
-        // float c, f;
-        // int f_bcd, f_int, f_dec;
-        // c = (meas.t_integral * 10) + meas.t_decimal;
-        // f = ((9.0/5.0) * c) + 320.0;
-        // printf("c: %f  f: %f\n", c, f);
-        // f_int = (int) f / 10;
-        // f_dec = (int) f % 10;
         dht22_c_to_f(&meas);
         sprintf(text, "F%02d%d", meas.t_integral, meas.t_decimal);
         ftseg_write_text(ftseg, 0, text);
@@ -246,53 +140,5 @@ int main(void)
         _delay_ms(DELAY_LOOP);
     }
    
-//     neopixel_stick_off(&np_stick);
-//     neopixel_refresh(&np_stick);
-//     pixels_mods_clear_all(&np_stick);
-// //    startup_anim();
-// //
-//     pixels_anim_init(&anim_startup, &pixels_anim_cb_flicker);
-//     pixels_anim_set_oneshot(&anim_startup);
-//     pixels_anim_enable_channels(&anim_startup, _BV(PIXELS_CH_BLUE) | _BV(PIXELS_CH_RED));
-//     pixels_anim_set_range(&anim_startup, 0, 0x80);
-//     pixels_anim_play(&anim_startup);
-
-//     pixels_anim_init(&anim_sine, &pixels_anim_cb_offset_table);
-//     pixels_anim_set_loop(&anim_sine);
-//     pixels_anim_enable_channels(&anim_sine, _BV(PIXELS_CH_GREEN));
-//     pixels_anim_set_table_id(&anim_sine, PIXELS_ANIM_SINEWAVE);
-//     pixels_anim_play(&anim_sine);
-    // while(1) {
-    //     pixels_anim_apply_state(&np_stick, &anim_startup);
-
-    //     pixels_anim_apply_state(&np_stick, &anim_sine);
-
-
-    //     neopixel_refresh(&np_stick);
-
 }
 
-// void startup_anim(void)
-// {
-//     pixels_anim anim_sine;
-
-//     neopixel_stick_off(&np_stick);
-//     neopixel_refresh(&np_stick);
-//     pixels_mods_clear_all(&np_stick);
-
-//     //_delay_ms(1000);
-
-//     /* Startup animation */
-//     pixels_anim_init(&anim_startup, &pixels_anim_cb_flicker);
-//     pixels_anim_set_oneshot(&anim_startup);
-//     pixels_anim_enable_channels(&anim_startup, _BV(PIXELS_CH_BLUE) | _BV(PIXELS_CH_GREEN));
-//     pixels_anim_set_range(&anim_startup, 0, 0x80);
-//     pixels_anim_play(&anim_startup);
-
-//     pixels_anim_init(&anim_sine, &pixels_anim_cb_offset_table);
-//     pixels_anim_set_loop(&anim_sine);
-//     pixels_anim_enable_channels(&anim_sine, _BV(PIXELS_CH_GREEN));
-//     pixels_anim_set_table_id(&anim_sine, PIXELS_ANIM_SINEWAVE);
-//     pixels_anim_play(&anim_sine);
-
-// }
